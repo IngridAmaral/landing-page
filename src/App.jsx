249f832463c5435471774f4fc43f5dfdaa7995e1
Ingrid Amaral <ingridamaralrj@gmail.com>
2021-01-15 13:06:43 +0100

    adds header with logic and tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
 import React from 'react';
+import Header from './components/header/Header';
 import './App.scss';
 
-const App = () => <div className="App">Hello</div>;
+export const HOME = 'Home';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+      selectedNavItem: HOME
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen
+    }));
+  };
+
+  selectNavItem = (item) => {
+    this.setState({ selectedNavItem: item });
+  };
+
+  render() {
+    const { isMenuOpen, selectedNavItem } = this.state;
+
+    return (
+      <div className="App">
+        <Header
+          toggleMenu={this.toggleMenu}
+          isMenuOpen={isMenuOpen}
+          selectNavItem={this.selectNavItem}
+          selectedNavItem={selectedNavItem}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;

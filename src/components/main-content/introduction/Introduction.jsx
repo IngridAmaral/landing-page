3379e650e59b37477beb4a95b93bf5dd7f451c1f
Ingrid Amaral <ingridamaralrj@gmail.com>
2021-01-16 14:15:44 +0100

    creates a introduction component and adds tests

---
+++ src/components/main-content/introduction/Introduction.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import './Introduction.scss';
+
+export const INTRODUCTION_TITLE = 'Welcome to your personal area.';
+export const INTRODUCTION_DESCRIPTION =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum elit id auctor mollis. Aenean feugiat commodo quam, vulputate viverra lorem iaculis in. Curabitur varius commodo lacus eget vestibulum. Curabitur vitae risus nec justo faucibus vulputate. Nunc blandit nisi lobortis, gravida elit non, tincidunt dolor. Mauris ullamcorper suscipit risus, eget dictum dui consequat sed. Nam quis orci id dui consectetur elementum. Nulla et lacus massa.';
+
+const Introduction = () => (
+  <div className="introduction-container">
+    <span className="introduction-title">{INTRODUCTION_TITLE}</span>
+    <span className="introduction-description">{INTRODUCTION_DESCRIPTION}</span>
+  </div>
+);
+
+// Introduction.propTypes = {
+// };
+
+export default Introduction;

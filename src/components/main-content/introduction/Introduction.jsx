314bd30eab0b1b6ba5083c9176a1fb64ece25102
Ingrid Amaral <ingridamaralrj@gmail.com>
2021-01-22 18:00:34 +0100

    moves consts to a separate file

--- src/components/main-content/introduction/Introduction.jsx
+++ src/components/main-content/introduction/Introduction.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import { INTRODUCTION_TITLE, INTRODUCTION_DESCRIPTION } from '../../../data';
 import './Introduction.scss';
-
-export const INTRODUCTION_TITLE = 'Welcome to your personal area.';
-export const INTRODUCTION_DESCRIPTION =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum elit id auctor mollis. Aenean feugiat commodo quam, vulputate viverra lorem iaculis in. Curabitur varius commodo lacus eget vestibulum. Curabitur vitae risus nec justo faucibus vulputate. Nunc blandit nisi lobortis, gravida elit non, tincidunt dolor. Mauris ullamcorper suscipit risus, eget dictum dui consequat sed. Nam quis orci id dui consectetur elementum. Nulla et lacus massa.';
 
 const Introduction = () => (
   <div className="introduction-container">
--- src/components/main-content/investment-options/InvestmentOptions.jsx
+++ src/components/main-content/investment-options/InvestmentOptions.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import InvestmentOption from './investment-option/InvestmentOption';
+import { INVESTMENT_OPTIONS } from '../../../data';
 import './InvestmentOptions.scss';
-
-export const INVESTMENT_OPTIONS = [
-  {
-    title: 'Investments',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum elit id auctor mollis. Aenean feugiat commodo quam, vulputate viverra lorem iaculis in. Curabitur varius commodo lacus eget vestibulum. Curabitur vitae risus nec justo faucibus vulputate.',
-    exploreButton: false
-  },
-  {
-    title: 'Stocks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum elit id auctor mollis.',
-    exploreButton: true
-  }
-];
 
 const InvestmentOptions = () => (
   <div className="investment-options-container">
-    {INVESTMENT_OPTIONS.map(({ title, description, exploreButton }) => (
+    {INVESTMENT_OPTIONS.map((option) => (
       <InvestmentOption
-        title={title}
-        description={description}
-        exploreButton={exploreButton}
-        key={`investment-option-${title}`}
+        option={option}
+        key={`investment-option-${option.title}`}
       />
     ))}
   </div>
